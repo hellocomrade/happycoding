7875dfddcf743f532173c8891b8e7898716774e7
hellocomrade <gwang@glc.org>
2015-12-03 19:08:20 +0000

    Update comment for MinAvgTwoSlice.cpp

--- MinAvgTwoSlice.cpp
+++ MinAvgTwoSlice.cpp
@@ -9,6 +9,22 @@ using namespace std;
 
 /*
  * Naive solution
+ *
+ * Since we deal with average here, at least a pair of elements should be examined each step
+ * From the example it gives, it's an easy observation that the ith and (i+1)th should be checked
+ * on each step. In [4,2,2], [2,2] has a smaller avg than [4,2]; However, it's also possible that
+ * 3 continuous elements could have an even lower avg, say [-8,-4,-10]. I don't have an example on top
+ * of my head to qualify tree elements min avg with all positive numbers.
+ *
+ * So, based upon the observation above, we will loop the array start from 1 (assuming we take A[0] and A[1]
+ * as the starting value already) and end at len-2. On each step, both i and i+1 th elements will be checked:
+ *
+ * 1. If (A[i]+A[i+1])/2.0 is smaller than the previous avg min; If yes, reset startidx to i and update avg min.
+ * 2. If current sum plus A[i] and then divided by the number of elements is smaller than previous avg min;
+ *    if yes, update avg min.
+ * 3  If neither of above is satisfied, we have found the min avg end at index i-1, reset current sum to max value
+ *    and then move i backward 1 to i-1, also reset number count to 1;
+ * 4. According to the above 3 steps, we now can update the avg min so far and start index of current avg-min sequence 
  */
 int solutionMinAvgTwoSlice(vector<int> &A)
 {
