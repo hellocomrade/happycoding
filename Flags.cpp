81221613721c1aa38bade35e9ca02bc20e0fc380
hellocomrade <gwang@glc.org>
2015-12-24 15:10:32 +0000

    Add comments for Flags

--- Flags.cpp
+++ Flags.cpp
@@ -8,6 +8,28 @@ using namespace std;
 /*
 https://codility.com/demo/results/trainingWUNYHU-BWR/
 https://codility.com/media/train/solution-flags.pdf
+Observation:
+1. There should be no more than sqrt(N) peaks in any sequence.
+2. If there is only one peak in the sequence, the default result should be 1.
+3. If taking k flags can put flags on m peaks, k-1, k-2 , K-3...1 can do m-1, m-2, m-3...1. So we are looking for the 
+   largest k that can get m flags onto peaks. If you take more than k flags, however, due to the constriction on the
+   fact that "the distance between any two flags should be greater than or equal to k", there will be less than m flags
+   on peaks.
+4. The upper boundary of k could be set to peaks[n-1] - peaks[0] coz you can only put 1 flags. The lower boundary is 2
+   if we ignore taking 1 flag, which is always doable. The low bound is 2 because the distance between peaks is always no
+   less than 2.
+5. It appears we can always greedily put a flag on the first peak.
+
+Applied the binary search here:
+
+We have a monotonic sequence here on the number of flags [2, peaks[n-1]-peaks[0]]. The condition to drop half of the sequence,
+however, is the fact that, if we take m flags over all peaks and have n flags left, we could take less flags and have less than n flags
+left after going through all peaks.
+
+Therefore, finding all peaks takes O(N) time and O(sqrt(N)) space. Conducting the binary search takes O(sqrt(N)*log(sqrt(N)) time. 
+Overall time complexity is O(N) + O(sqrt(N)*log(sqrt(N))
+Since sqrt(N) is greater than log(sqrt(N)), we could have  O(sqrt(N)*log(sqrt(N)) < O(sqrt(N)*sqrt(N)) = O(N). So the overall time complexity
+is O(N)
 */
 int solutionFlags(vector<int> &A)
 {
