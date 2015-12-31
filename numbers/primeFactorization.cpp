78dd46f24aabe5ee07a54c802c1d92cbb68eea39
Hello Comrade <gwang@glc.org>
2015-12-31 12:26:52 -0500

    Add prime factorization through Sieve lookup

--- numbers/primeFactorization.cpp
+++ numbers/primeFactorization.cpp
@@ -1,4 +1,5 @@
 #include <cassert>
+#include <vector>
 #include <iostream>
 
 using namespace std;
@@ -35,6 +36,48 @@ int countPrimeFactorization(int N)
 		++count;
 	return count;
 }
+vector<int> getSmallestPrimeDivisorLookup(int max)
+{
+	assert(max > 0);
+	vector<int> lookup(max + 1, 0);
+	int i = 2, k;
+	while (i*i <= max)
+	{
+		if (0 == lookup[i])
+		{
+			k = i * i;
+			while (k <= max)
+			{
+				lookup[k] = i;
+				k += i;
+			}
+		}
+		++i;
+	}
+	return lookup;
+}
+/*
+https://codility.com/media/train/9-Sieve.pdf
+each composite number has at least one prime divisor
+*/
+vector<int> primeFactorization(int N, const vector<int> &lookup)
+{
+	assert(N > 1 && lookup.size() >= N);
+	vector<int> pfactors;
+	while (0 != lookup[N])
+	{
+		pfactors.push_back(lookup[N]);
+		N /= lookup[N];
+	}
+	pfactors.push_back(N);
+	return pfactors;
+}
+void printPrimeFactorization(const vector<int> &A)
+{
+	for (int p : A)
+		cout << p << ',';
+	cout << endl;
+}
 void testPrime()
 {
 	cout << "Expect 1: " << countPrimeFactorization(8) << endl;
@@ -42,4 +85,13 @@ void testPrime()
 	cout << "Expect 1: " << countPrimeFactorization(49) << endl;
 	cout << "Expect 2: " << countPrimeFactorization(36) << endl;
 	cout << "Expect 2: " << countPrimeFactorization(103 * 373) << endl;
+
+	vector<int> lookup = getSmallestPrimeDivisorLookup(40000);
+	printPrimeFactorization(primeFactorization(20, lookup));
+	printPrimeFactorization(primeFactorization(36, lookup));
+	printPrimeFactorization(primeFactorization(47, lookup));
+	printPrimeFactorization(primeFactorization(2, lookup));
+	printPrimeFactorization(primeFactorization(8, lookup));
+	printPrimeFactorization(primeFactorization(49, lookup));
+	printPrimeFactorization(primeFactorization(103 * 373, lookup));
 }
