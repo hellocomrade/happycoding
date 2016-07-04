bbef05bc1c26ac712f7b9a8a014bea319d57afaf
Hello Comrade <hellocomrade@users.noreply.github.com>
2016-07-04 00:00:27 -0400

    Create BSTLevelOrderTraversal.py

---
+++ python/BSTLevelOrderTraversal.py
@@ -0,0 +1,32 @@
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+
+class Solution:
+    """
+    @param root: The root of binary tree.
+    @return: Level order in a list of lists of integers
+    """
+    def levelOrder(self, root):
+        # write your code here
+        ans = []
+        if root is not None:
+            q = []
+            q.append(root)
+            while q:
+                size = len(q)
+                vec = [0] * size
+                for i in xrange(size):
+                    node = q.pop(0)
+                    vec[i] = node.val
+                    if node.left:
+                        q.append(node.left)
+                    if node.right:
+                        q.append(node.right)
+                ans.append(vec)
+        return ans
