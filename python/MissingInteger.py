11a86127a900c4f0eb850b66b24f5290a9055ddd
Hello Comrade <hellocomrade@users.noreply.github.com>
2016-07-22 14:46:44 -0400

    Create MissingInteger.py

---
+++ python/MissingInteger.py
@@ -0,0 +1,12 @@
+#https://codility.com/demo/results/training4NGMVM-MQT/
+#Ref: pigeonhole principle 
+def solutionMissingInteger(A):
+    size = len(A)
+    B = [0] * (size + 1)
+    for i in xrange(size):
+        if A[i] > 0 and A[i] <= size:
+            B[A[i]] = 1
+    for i in xrange(1, size + 1):
+        if B[i] == 0:
+            return i
+    return size + 1
