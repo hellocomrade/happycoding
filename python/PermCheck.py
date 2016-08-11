63c734fbc64f942d6e55deec0e5b8fd1ef4ad112
Hello Comrade <hellocomrade@users.noreply.github.com>
2016-08-11 10:26:14 -0400

    Create PermCheck.py

---
+++ python/PermCheck.py
@@ -0,0 +1,10 @@
+#https://codility.com/demo/results/trainingBTGWMG-N4X/
+#
+#no need for the extra O(N) space, it can be done with O(1) space and O(N) time
+def solution(A):
+    size = len(A)
+    for i in xrange(size):
+        while A[i] != i + 1:
+            if A[i] > size or A[A[i] - 1] == A[i]: return 0
+            A[A[i] - 1], A[i] = A[i], A[A[i] - 1]
+    return 1
