3fb7eff6bb8be72f6b589cdd1823d37c065c6444
Hello Comrade <hellocomrade@users.noreply.github.com>
2016-09-19 09:25:32 -0400

    Update JumpGame2.java

--- java/JumpGame2.java
+++ java/JumpGame2.java
@@ -17,17 +17,26 @@ public class SolutionJumpGame2 {
         }
         return ans;
     }
+    //This version has extra complication comparing with above
     public int jump1(int[] A) {
         int len = A.length, ans = 0;
         if(len > 1) {
             int maxstep = A[0], cnt = A[0];
             for(int i = 1; i < len; ++i) {
+                /*
+                 * This exit condition might be confusing: why len not len - 1?
+                 * Well, the cnt is checked afterward: after it's supposed to be checked when it is assigned at line 34.
+                 * so, variable i here when we check, it's already 1 position off the "i" we are supposed to check.
+                 * The condition is actually: cnt + i - 1 >= len - 1 ==> cnt + i >= len
+                 */
                 if(cnt + i >= len)return ++ans;
                 --cnt;--maxstep;
                 maxstep = Math.max(A[i], maxstep);
                 if(0 == cnt) {
-                    if(maxstep <= 0)return -1;
+                    if(maxstep == cnt)return -1;
                     cnt = maxstep;
+                    //maxstep should be reset?
+                    maxstep = 0;
                     ++ans;
                 }
             }
