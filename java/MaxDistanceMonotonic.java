fbd818207853052e35a5147761c45cdf4aee965b
Hello Comrade <hellocomrade@users.noreply.github.com>
2017-01-26 16:27:44 -0500

    Update MaxDistanceMonotonic.java

--- java/MaxDistanceMonotonic.java
+++ java/MaxDistanceMonotonic.java
@@ -2,7 +2,7 @@
 
 /*
 This is actually a O(NlogN) solution, but Codility say otherwise:) They need better, longer test cases.
-The O(N) solution will be added later.
+The O(N) solution will be added later. So it's here:https://codility.com/demo/results/training8NVKBY-RE8/
 */
 import java.util.Collections;
 import java.util.ArrayList;
@@ -15,7 +15,23 @@ class SolutionMaxDistanceMonotonic {
             this.idx = idx;
         }    
     }
+    //O(N)
     public int solution(int[] A) {
+        int ans = 0;
+        Stack<Pair> stk = new Stack<>();
+        stk.push(new Pair(A[A.length - 1], A.length - 1));
+        for(int i = A.length - 2; i > -1; --i)
+            if(A[i] > stk.peek().val)stk.push(new Pair(A[i], i));
+        for(int i = 0; i < A.length && !stk.empty(); ++i) {
+            while(false == stk.empty() && A[i] <= stk.peek().val) {
+                ans = Math.max(ans, stk.peek().idx - i);
+                stk.pop();
+            }
+        }
+        return ans;
+    }
+    //O(NlogN)
+    public int solution1(int[] A) {
         int ans = 0, min = A.length;
         ArrayList<Solution.Pair> lst = new ArrayList<>();
         for(int i = 0; i < A.length; ++i)
