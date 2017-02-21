929e3e743460d85a21446dc061b72c8f8f27e3f4
hellocomrade <sun8726@hotmail.com>
2017-02-20 21:37:12 -0500

    Add Leetcode 115

--- leetcode/DistinctSubsequences.cpp
+++ leetcode/DistinctSubsequences.cpp
@@ -7,6 +7,8 @@ using namespace std;
 
 //https://leetcode.com/problems/distinct-subsequences/
 /*
+ 115. Distinct Subsequences
+ 
  Given a string S and a string T, count the number of distinct subsequences of T in S.
  
  A subsequence of a string is a new string which is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (ie, "ACE" is a subsequence of "ABCDE" while "AEC" is not).
