d3e4bb095dbcdc9aec3b7d61db6b875b4ac9afc8
Hello Comrade <hellocomrade@users.noreply.github.com>
2017-03-03 21:35:45 -0500

    Update leetcode 32

--- leetcode/LongestValidParentheses.cpp
+++ leetcode/LongestValidParentheses.cpp
@@ -32,6 +32,29 @@ valid parentheses with the possible valid ones down the road because of this bar
 
 Since we reset last in the midde of every iteration, ans has to be examined at the beginning of each iteration, as well as after exit of the loop since
 it's possible to have a valid parentheses reaches the end of the string. The value is stored in last and will not be examined on ans since the loop is done.
+
+
+
+OK, here we go the "DP" version: I was about to give up on DP since this is really not DP for me. However, leetcode setup a pretty tough test case that
+a Java with stack will get a TLE even I replaced stack with deque...
+Here is how "DP" works: create a memo array with with zero. If we meet a '(' at index i, we leave 0 at memo[i], if we see a ')', check
+the follow conditions:
+1. if the previous char is '(', then we can count 2 at least for memo[i];
+2. if the previous char is ')', say '(())', i = 3, previous index i = 2. In this case, memo[2] = 2, ')' with index i = 3 will actually be
+   checked against index i - memo[i - 1] - 1 = 0. By doing so, we solve the nested parentheses;
+
+With the above 2 steps, we count the total from '(' till matched ')', for the example '(())', we have the count from index 0 to index 3;
+
+3. Now, we need to find out if there is a valid parentheses ends just before the '(' we checked. Say '()(())' and we are at index i = 5.
+   In this case, we first get the length from i = 2 to 5. Then move the index one step backward, we check index i = 1. If the char at
+   index i = 1 is ')', then we need to add the length in memo[1] onto memo[5].
+   
+Adding 3 steps together, we have memo[i] = 2 + memo[i - 1] + memo[i - memo[i - 1] - 2] if and only if ')' == s[i] && i - memo[i - 1] - 1 >= 0 && '(' == s[i - memo[i - 1] - 1]
+Of course, if i - memo[i - 1] - 2 < 0, then no need to add it. Also, no need to check the char at index = i - memo[i - 1] - 2. Why? If it's '(',
+then memo[i - memo[i - 1] - 2] == 0
+
+In each iteration, we compare memo[i] with ans and keep the max in ans. This is a O(N) time and O(N) space algorithm. I would rather call
+it a memorization algorithm instead of DP though...
 */
 class SolutionLongestValidParentheses {
 public:
