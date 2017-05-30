0bc629398830e92ef0f7229ca9a2ec06702e20a3
Hello Comrade <sun8726@hotmail.com>
2017-05-30 12:02:20 -0400

    Update comment on leetcode 139

--- leetcode/WordBreak.cpp
+++ leetcode/WordBreak.cpp
@@ -20,6 +20,26 @@ Return true because "leetcode" can be segmented as "leet code".
 
 UPDATE (2017/1/4):
 The wordDict parameter had been changed to a list of strings (instead of a set of strings). Please reload the code definition to get the latest changes.
+
+Observation:
+Classic entry level DP question. O(N^2)
+
+In order to check whether string s from index 0 to i can be split into words in wordDict, we have to try
+
+[0-1, 2-i], [0-2, 3-i],... ,[0-(i-1),i]
+
+Each of above can be further split into small sections, which obviously generates lots of duplicates that can be memorized up front.
+
+If we do this bottom up, we will use two pointers, one, i, to mark the end of the index (from 0 to len(str) - 1); the other one, j, is used to
+move between [0, i], so str in [0, i] can be split into [0, j - 1], [j, i]. If such a j exists that str[j + 1, i] matches a word in dict and str[0, j - 1]
+can be built using wordDict (this is checked using memo array), we know str[0, i] can be built using wordDict, then this info is kept in memo array.
+
+In order to find word in dict faster, we could put wordDict onto a hashset, which gives us O(1) lookup time.
+
+You may get a TLE if you just implment through the above appraoch. The catch is: you don't have to examine all possible j if (i - j) is larger than the longest
+word in wordDict. By doing so, certain test cases can be completed earlier.
+
+The boundary condition is tricky if s is allowed to be empty. An extra space in memo is used to cover this.
 */
 class SolutionWordBreak {
 public:
