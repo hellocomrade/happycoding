cef5675a3941722599f0141ee89cb6b6818ce4d0
Hello Comrade <hellocomrade@users.noreply.github.com>
2017-06-17 18:23:59 -0400

    Update leetcode 21

--- leetcode/mergeTwoLists.cpp
+++ leetcode/mergeTwoLists.cpp
@@ -1,3 +1,10 @@
+//https://leetcode.com/problems/merge-two-sorted-lists/
+
+/*
+21. Merge Two Sorted Lists
+
+Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+*/
 namespace mergeTwoLists{
 
 struct ListNode { 
@@ -8,7 +15,28 @@ struct ListNode {
  
 class SolutionMergeTwoLists {
 public:
+    /*
+    using a dummy node to make sure pos has the init value then we could simplify the logic in the loop a bit by not checking
+    if pos is nullptr
+    */ 
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode ans(0);
+        ListNode *pos = &ans;
+        while(nullptr != l1 && nullptr != l2) {
+            if(l1->val <= l2->val) {
+                pos->next = l1;
+                l1 = l1->next;
+            }
+            else {
+                pos->next = l2;
+                l2 = l2->next;
+            }
+            pos = pos->next;
+        }
+        pos->next = nullptr != l1 ? l1 : l2;
+        return ans.next;
+    }
+    ListNode* mergeTwoLists1(ListNode* l1, ListNode* l2) {
         if(nullptr == l1)return l2;
         if(nullptr == l2)return l1;
         ListNode *head;
@@ -24,7 +52,7 @@ public:
         }
         return head;
     }
-    ListNode* mergeTwoLists1(ListNode* l1, ListNode* l2) {
+    ListNode* mergeTwoLists2(ListNode* l1, ListNode* l2) {
         if(nullptr == l1)return l2;
         if(nullptr == l2)return l1;
         ListNode *head = nullptr;
