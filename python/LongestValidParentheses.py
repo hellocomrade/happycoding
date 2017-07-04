2bdc9ff4ff400d865fb6c2628d0d481244c1ac93
Hello Comrade <hellocomrade@users.noreply.github.com>
2017-07-03 23:38:05 -0400

    Update LongestValidParentheses.py

--- python/LongestValidParentheses.py
+++ python/LongestValidParentheses.py
@@ -8,6 +8,19 @@ class SolutionLongestValidParentheses(object):
         size, ans = len(s), 0
         memo = [0] * size
         for i in range(1, size):
+            if ')' == s[i] and i - memo[i - 1] > 0 and '(' == s[i - memo[i - 1] - 1]:
+                memo[i] = memo[i - 1] + 2 + (memo[i - memo[i - 1] - 2] if i - memo[i - 1] > 1 else 0)
+                ans = max(ans, memo[i])
+        return ans
+    
+    def longestValidParentheses1(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        size, ans = len(s), 0
+        memo = [0] * size
+        for i in range(1, size):
             if ')' == s[i] and i - memo[i - 1] - 1 >= 0 and '(' == s[i - memo[i - 1] - 1]:
                 memo[i] = memo[i - 1] + 2
                 if i - memo[i - 1] - 2 >= 0:
