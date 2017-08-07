108c4856ffb4dbe2565956079aff6f000ed73797
Hello Comrade <sun8726@hotmail.com>
2017-08-07 10:33:59 -0400

    Add leetcode 4, not done yet

---
+++ leetcode/MedianOfTwoSortedArrays.cpp
@@ -0,0 +1,176 @@
+#include <cassert>
+#include <cstdlib>
+#include <vector>
+#include <algorithm>
+#include <limits>
+
+using namespace std;
+
+//https://leetcode.com/problems/median-of-two-sorted-arrays/
+/*
+4. Median of Two Sorted Arrays
+
+There are two sorted arrays nums1 and nums2 of size m and n respectively.
+
+Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+
+Example 1:
+nums1 = [1, 3]
+nums2 = [2]
+
+The median is 2.0
+Example 2:
+nums1 = [1, 2]
+nums2 = [3, 4]
+
+The median is (2 + 3)/2 = 2.5
+
+Observations:
+Since it asks a logarithmic algorithm and both arrays are sorted, binary search indeed. I managed to carry out the first
+version in "findMedianSortedArrays1", which basically counts where the median is. Note that the median could be in either array.
+Therefore, the search has to conduct twice if the total length is odd or even four times if even. Well, once it's done,
+I realize the total time complexity is log(M) * log(N) since BS takes log time so does lower_bound and upper_bound.
+
+Therefore, in order to achieve a faster algorithm, we have to finish BS partition in O(1)! There goes the second version:
+
+Since our idea is to find the element that has exactly targetCount elements before it and both arrays are sorted. Once we locate
+the mid, we could directly comparing num[mid] with targetArr[targetCount - mid - 1] and targetArr[targetCount - mid] and it shall meet
+targetArr[targetCount - mid - 1] <= num[mid] <= targetArr[targetCount - mid].
+This means num[mid] is the targetCount element in both arrays combined.
+
+The complication about this approach is when targetCount - mid - 1/targetCount - mid are out of range of targetArr. Edge cases have to be
+carefully checked and they are messy... I tried to make BS looks nicer but the if..else before it doesn't look elegant at all...This is all
+because we compare a number with a range...
+
+There is a better explanation for this approach at: http://www.drdobbs.com/parallel/finding-the-median-of-two-sorted-arrays/240169222?pgno=2
+
+Wait, actually the time complexity is log(M) + log(N) since we have to search both arrays!
+
+Still not log(M + N) and there is actually a log(MIN(M, N)) version...
+*/
+class SolutionMedianOfTwoSortedArrays {
+private:
+	//O(log(M*N))
+	vector<int>::const_iterator search1(const vector<int>& num, const vector<int>& targetArr, size_t targetCount) {
+		auto lower = num.begin(), higher = num.end() - 1, mid = num.begin(), t = num.end(), t1 = num.end();
+		size_t count, count1;
+		while (lower <= higher && num.end() != lower && num.end() != higher) {
+			mid = lower + (higher - lower) / 2;
+			//Since we didn't do BS, have to use both lower_bound and upper_bound to cover
+			//the case that all elements in between are identical
+			t = lower_bound(targetArr.begin(), targetArr.end(), *mid);
+			t1 = upper_bound(targetArr.begin(), targetArr.end(), *mid);
+			count = mid - num.begin() + (targetArr.end() == t ? targetArr.size() : t - targetArr.begin());
+			count1 = mid - num.begin() + (targetArr.end() == t1 ? targetArr.size() : t1 - targetArr.begin());
+			if (targetCount >= count && targetCount <= count1)return mid;
+			else if (targetCount < count)
+				higher = num.begin() != mid ? mid - 1 : num.end();
+			else
+				lower = num.end() != mid ? mid + 1 : num.end();
+		}
+		return num.end();
+	}
+	//O(log(N))
+	int search(const vector<int>& num, const vector<int>& targetArr, int targetCount) {
+		int lower = 0, higher = (int)num.size() - 1, mid = 0, tsize = (int)targetArr.size();
+		long long t1, t2;
+		if (higher < 0 || targetCount < 0)return -1;
+		else if (0 == tsize)return targetCount <= higher ? targetCount : -1;//This is for the case that one of the array is empty
+		while (lower <= higher) {
+			mid = lower + (higher - lower) / 2;
+			if (targetCount - mid - 1 < 0) {
+				t1 = numeric_limits<int>::min();
+				t2 = (0 == targetCount - mid) ? targetArr[0] : t1;
+			}
+			else if (targetCount - mid >= tsize) {
+				t2 = numeric_limits<int>::max();
+				t1 = (tsize == targetCount - mid) ? targetArr[tsize - 1] : t2;
+			}
+			else {
+				t1 = targetArr[targetCount - mid - 1];
+				t2 = targetArr[targetCount - mid];
+			}
+			if (num[mid] >= t1 && num[mid] <= t2)return mid;
+			else if (num[mid] < t1)lower = mid + 1;
+			else higher = mid - 1;
+			//Original: may be better for readability in terms of thinking process
+			/*if (targetCount - mid - 1 < 0) {
+			if (mid == targetCount && num[mid] <= targetArr[0])return mid;
+			else higher = mid - 1;
+			}
+			else if (targetCount - mid <= tsize && targetArr[targetCount - mid - 1] <= num[mid]) {
+			if ((targetCount - mid == tsize || targetArr[targetCount - mid] >= num[mid]))return mid;
+			else higher = mid - 1;
+			}
+			else lower = mid + 1;*/
+		}
+		return -1;
+	}
+public:
+	//O(log(M) + log(N))
+	double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+		double ans = 0.0;
+		size_t len1 = nums1.size(), len2 = nums2.size();
+		if (0 == len1 && 0 == len2)return 0.0;
+		size_t targetCnt = std::min(len1, len2) + abs((int)(len1 - len2)) / 2;
+		long long total = (long long)len1 + len2;
+		if (1 == (total & 1)) {
+			int idx1 = this->search(nums1, nums2, (int)targetCnt);
+			int idx2 = this->search(nums2, nums1, (int)targetCnt);
+			ans = -1 == idx1 ? nums2[idx2] : nums1[idx1];
+		}
+		else {
+			int idxa = this->search(nums1, nums2, (int)targetCnt - 1);
+			int idxb = this->search(nums1, nums2, (int)targetCnt);
+			int ma = -1 != idxa ? nums1[idxa] : nums2[this->search(nums2, nums1, (int)targetCnt - 1)];
+			int mb = -1 != idxb ? nums1[idxb] : nums2[this->search(nums2, nums1, (int)targetCnt)];
+			ans = (ma + mb) / 2.0;
+		}
+		return ans;
+	}
+	//O(log(M*N))
+	double findMedianSortedArrays1(const vector<int>& nums1, const vector<int>& nums2) {
+		double ans = 0.0;
+		size_t len1 = nums1.size(), len2 = nums2.size();
+		if (0 == len1 && 0 == len2)return ans;
+		else if (0 == len2)ans = (1 == (len1 & 1)) ? nums1[len1 / 2] : nums1[len1 / 2 - 1] + (nums1[len1 / 2] - nums1[len1 / 2 - 1]) / 2.0;
+		else if (0 == len1)ans = (1 == (len2 & 1)) ? nums2[len2 / 2] : nums2[len2 / 2 - 1] + (nums2[len2 / 2] - nums2[len2 / 2 - 1]) / 2.0;
+		else {
+			size_t targetCnt = std::min(len1, len2) + abs((int)(len1 - len2)) / 2;
+			long long total = (long long)len1 + len2;
+			if (1 == (total & 1)) {
+				auto itor1 = this->search1(nums1, nums2, targetCnt);
+				auto itor2 = this->search1(nums2, nums1, targetCnt);
+				ans = nums1.end() == itor1 ? *itor2 : *itor1;
+			}
+			else {
+				auto itor1 = this->search1(nums1, nums2, targetCnt - 1);
+				auto itor2 = this->search1(nums1, nums2, targetCnt);
+				if (nums1.end() == itor1)
+					itor1 = this->search1(nums2, nums1, targetCnt - 1);
+				if (nums1.end() == itor2)
+					itor2 = this->search1(nums2, nums1, targetCnt);
+				ans = ((double)*itor1 + (double)*itor2) / 2.0;
+			}
+		}
+		return ans;
+	}
+};
+void TestMedianOfTwoSortedArrays() {
+	SolutionMedianOfTwoSortedArrays so;
+	assert(9.0 == so.findMedianSortedArrays(vector<int>{3, 4, 7, 9, 12, 19, 28}, vector<int>{2, 5, 18, 21}));
+	assert(0.0 == so.findMedianSortedArrays(vector<int>{}, vector<int>{}));
+	assert(1.0 == so.findMedianSortedArrays(vector<int>{1, 1}, vector<int>{1, 2}));
+	assert(2.0 == so.findMedianSortedArrays(vector<int>{1, 3}, vector<int>{1, 3}));
+	assert(2.5 == so.findMedianSortedArrays(vector<int>{1, 4}, vector<int>{2, 3}));
+	assert(2.5 == so.findMedianSortedArrays(vector<int>{1, 2, 2, 3, 3, 4}, vector<int>{1, 2, 2, 4, 4, 4}));
+	assert(2.5 == so.findMedianSortedArrays(vector<int>{1}, vector<int>{2, 3, 4}));
+	assert(1.5 == so.findMedianSortedArrays(vector<int>{}, vector<int>{1, 2}));
+	assert(6.0 == so.findMedianSortedArrays(vector<int>{6}, vector<int>{}));
+	assert(2.0 == so.findMedianSortedArrays(vector<int>{1, 3}, vector<int>{2}));
+	assert(2.5 == so.findMedianSortedArrays(vector<int>{1, 2}, vector<int>{3, 4}));
+	assert(2.0 == so.findMedianSortedArrays(vector<int>{1, 2, 2, 2}, vector<int>{3, 3, 4}));
+	assert(2.5 == so.findMedianSortedArrays(vector<int>{1, 2, 2}, vector<int>{3, 3, 4}));
+	assert(6.0 == so.findMedianSortedArrays(vector<int>{1, 2, 6}, vector<int>{100, 200}));
+	assert(10.0 == so.findMedianSortedArrays(vector<int>{1, 2, 3, 5, 8, 12, 19}, vector<int>{4, 10, 13, 19, 19, 20}));
+}
