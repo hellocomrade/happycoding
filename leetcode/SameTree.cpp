7c2b74ac9ef25da993591b10c24a5dbb777cd002
Hello Comrade <sun8726@hotmail.com>
2017-12-01 11:02:53 -0500

    Replace NULL with nullptr

--- leetcode/SameTree.cpp
+++ leetcode/SameTree.cpp
@@ -55,7 +55,7 @@ namespace SameTree {
 		int val;
 		TreeNode *left;
 		TreeNode *right;
-		TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+		TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
 	};
 
 	class Solution {
