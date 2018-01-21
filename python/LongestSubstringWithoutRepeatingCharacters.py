750b3872d50b5aace648a3e32e91125f7dc4085a
Hello Comrade <hellocomrade@users.noreply.github.com>
2018-01-20 21:51:14 -0500

    Update LongestSubstringWithoutRepeatingCharacters.py

--- python/LongestSubstringWithoutRepeatingCharacters.py
+++ python/LongestSubstringWithoutRepeatingCharacters.py
@@ -5,6 +5,18 @@ class SolutionLongestSubstringWithoutRepeatingCharacters(object):
         :type s: str
         :rtype: int
         """
+        memo, l, r, ans = [0] * 256, 0, 0, 0
+        while r < len(s):
+            l = max(memo[ord(s[r])], l)
+            ans = max(r - l + 1, ans)
+            memo[ord(s[r])], r = r + 1, r + 1
+        return ans
+    
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
         memo, start, ans = dict(), 0, 0
         for i in range(len(s)):
             if s[i] in memo and (memo[s[i]] >= start):
