1cf58a02d3e604d525c4571a754bda00da5995c5
Hello Comrade <sun8726@hotmail.com>
2018-01-29 10:56:51 -0500

    Update on leetcode 134

--- leetcode/GasStation.cpp
+++ leetcode/GasStation.cpp
@@ -17,16 +17,43 @@ The solution is guaranteed to be unique.
 
 Observation:
 Greedy. Although it's saying the array forms a circle, there is really no need to consider it at all.
-The rulle of thumb is: if the sum of gas is less than the sum of costs, you are definitely not going to make it.
+The rule of thumb is: if the sum of gas is less than the sum of costs, you are definitely not going to make it.
 Therefore, we simply start from index 0 and keep tracking the diff between sum of gas so far versus sum of costs so far;
 At meantime, we track if current gas in car is going to make to the next station. If yes, we move on. If not,
 we reset the gas tank to 0 and start point to i + 1, so we will try to begin our journey from the next station.
+
+Why gas tank can be reset to 0? At index i, is it possible to have gasInTank < 0 but gas[i] - cost[i] > 0?
+NO, coz before reaching index i, gasInTank is at least 0, the only reason gasInTank is less than 0 is because gas[i] - cost[i] < 0.
+Therefore, it's safe to reset gasInTank to 0 if it's less than 0.
+
+BTW, leetcode official test code failed on the following case:
+[5,1,6,1,10]
+[3,4,3,6,1]
+
+The answer is 4, but leetcode gives 2.
 */
 class SolutionGasStation {
 public:
-	//This version looks bettr to me
+	/*
+	Almost as same as canCompleteCircuit1, for the case:
+	[], []
+	leetcode returns -1, therefore correct that here.
+	*/
 	int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-		size_t len = gas.size();
+		int len = (int)gas.size(), ans = 0;
+		long long gasInTank = 0, total = 0;
+		for (int i = 0; i < len; total += gas[i] - cost[i], ++i) {
+			gasInTank += gas[i] - cost[i];
+			if (gasInTank < 0) {
+				gasInTank = 0;
+				ans = i + 1;
+			}
+		}
+		return total < 0LL || len < 1 ? -1 : ans;
+	}
+	//This version looks bettr to me
+	int canCompleteCircuit1(vector<int>& gas, vector<int>& cost) {
+		int len = (int)gas.size();
 		long long total = 0, sumSofar = 0;
 		int start = 0;
 		for (int i = 0; i < len; ++i) {
@@ -39,7 +66,7 @@ public:
 		}
 		return total < 0 ? -1 : (int)start;
 	}
-	int canCompleteCircuit1(vector<int>& gas, vector<int>& cost) {
+	int canCompleteCircuit2(vector<int>& gas, vector<int>& cost) {
 		int len1 = gas.size();
 		int len2 = cost.size();
 		if (len1 < 1 || len2 < 1 || len1 != len2)return -1;
