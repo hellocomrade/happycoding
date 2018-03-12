6d6658f897acc9a3938e50560a726a4670fc0e00
Hello Comrade <hellocomrade@users.noreply.github.com>
2018-03-12 12:35:03 -0400

    Add leetcode 155 in scala

---
+++ scala/MinStack.scala
@@ -0,0 +1,24 @@
+package leetcode.MinStack
+
+import scala.math.min
+
+//https://leetcode.com/problems/min-stack/
+class MinStack {
+    private var stk: List[(Int, Int)] = List()
+  
+    def push(x: Int) {
+        stk = (if(true == stk.isEmpty) x else min(x, stk(0)._1), x)::stk
+    }
+  
+    def pop() {
+        stk = stk.drop(0)   
+    }
+  
+    def top(): Int = {
+        stk(0)._2 
+    }
+    
+    def getMin(): Int = {
+        stk(0)._1
+    }
+}
