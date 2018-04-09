c3d93077195862a7287258bfbd1405456fd67bbf
Hello Comrade <hellocomrade@users.noreply.github.com>
2018-04-09 16:50:50 -0400

    Create SuperReducedString.py

---
+++ python/SuperReducedString.py
@@ -0,0 +1,6 @@
+#!/bin/python3
+from functools import reduce
+
+def super_reduced_string(s):
+    ans = reduce(lambda str, c : str + c if 0 == len(str) or str[-1] != c else str[:-1] if len(str) > 0 else "", s)
+    return ans if len(ans) > 0 else "Empty String"
