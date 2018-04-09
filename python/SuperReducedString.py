cc81338f34b7b5f7032d65a8e4ee08eb3a5598d8
Hello Comrade <hellocomrade@users.noreply.github.com>
2018-04-09 16:51:12 -0400

    Update SuperReducedString.py

--- python/SuperReducedString.py
+++ python/SuperReducedString.py
@@ -1,6 +1,7 @@
 #!/bin/python3
 from functools import reduce
 
+# https://www.hackerrank.com/challenges/reduced-string/
 def super_reduced_string(s):
     ans = reduce(lambda str, c : str + c if 0 == len(str) or str[-1] != c else str[:-1] if len(str) > 0 else "", s)
     return ans if len(ans) > 0 else "Empty String"
