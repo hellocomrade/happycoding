f81c62e0850ab809cf903400ff647a13e8d4f4fc
Hello Comrade <sun8726@hotmail.com>
2018-04-12 16:44:46 -0400

    Update leetcode 55

--- leetcode/JumpGame.cpp
+++ leetcode/JumpGame.cpp
@@ -22,22 +22,38 @@ Observation:
 Kind of greedy, not DP for sure. Always pick the largest possible moves at each step.
 Current move is consumed at each step by 1 and then compare with the available move at index i, the larger
 one will be set as the current move.
+
+Update on 4/12/2018:
+
+- Test cases don't allow empty array
+
+- If you do DP, you will get stack overflow or TLE:
+
+https://leetcode.com/problems/jump-game/solution/
 */
 class SolutionJumpGame {
 public:
-    bool canJump(std::vector<int>& nums) {
-	int len = nums.size();
-	if (0 == len)return false;
-	if (1 == len)return true;
-	if (0 == nums[0])return false;
-	int move = nums[0];
-	//we have an ealier break condition: if remaining move is enough to reach the last index
-	//directly, no need to loop anymore
-	for (int i = 1; i < len && move < len - i; ++i)
-	{
-	    move = std::max(move - 1, nums[i]);
-	    if (0 == move && i != len - 1)return false;
+	bool canJump(std::vector<int>& nums) {
+		int len = (int)nums.size(), nextMax = 0;
+		for (int i = 0;i < len; ++i) {
+			if (i > 0 && i > nextMax) return false;
+			nextMax = std::max(nextMax, i + nums[i]);
+		}
+		return true;
 	}
-	return true;
-    }
+	bool canJump1(std::vector<int>& nums) {
+		int len = nums.size();
+		if (0 == len)return false;
+		if (1 == len)return true;
+		if (0 == nums[0])return false;
+		int move = nums[0];
+		//we have an ealier break condition: if remaining move is enough to reach the last index
+		//directly, no need to loop anymore
+		for (int i = 1; i < len && move < len - i; ++i)
+		{
+			move = std::max(move - 1, nums[i]);
+			if (0 == move && i != len - 1)return false;
+		}
+		return true;
+	}
 };
