189549b0b4c8b3aecc5f8f1d3f2b4dbb1bd0de96
Hello Comrade <sun8726@hotmail.com>
2018-05-03 14:32:03 -0400

    Update leetcode 34

--- leetcode/Search4Range.cpp
+++ leetcode/Search4Range.cpp
@@ -31,22 +31,32 @@ private:
 			if (nums[m] == target)
 			{
 				ret = m;
-				if (isLeft)
-					h = m - 1;
-				else
-					l = m + 1;
-				continue;
+				if (isLeft) h = m - 1;
+				else l = m + 1;
 			}
-			if (nums[m] < target)
+			else if (nums[m] < target)
 				l = m + 1;
 			else
 				h = m - 1;
 		}
 		return ret;
 	}
+	int bs1(const vector<int>& nums, int target, int l, int h, bool isLeft) {
+		int m = 0;
+		while (l <= h) {
+			m = l + (h - l) / 2;
+			if (target == nums[m]) {
+				if (true == isLeft) h = m - 1;
+				else l = m + 1;
+			}
+			else if (target < nums[m]) h = m - 1;
+			else l = m + 1;
+		}
+		return true == isLeft ? l : h;
+	}
 public:
 	vector<int> searchRange(vector<int>& nums, int target) {
-		int len = nums.size(), idx = -1, lidx = -1, ridx = -1;
+		int len = (int)nums.size();
 		vector<int> range(2, -1);
 		if (len > 0)
 		{
@@ -54,5 +64,32 @@ public:
 			range[1] = this->bs(nums, target, 0, len - 1, false);
 		}
 		return range;
+	}
+	vector<int> searchRange1(vector<int>& nums, int target) {
+		int len = (int)nums.size();
+		vector<int> ans{ -1, -1 };
+		ans[0] = this->bs(nums, target, 0, len - 1, true);
+		ans[1] = this->bs(nums, target, 0, len - 1, false);
+		if (ans[1] < ans[0]) ans[0] = -1, ans[1] = -1;
+		return ans;
+	}
+	//Doing one BS on left and one BS on right
+	vector<int> searchRange0(vector<int>& nums, int target) {
+		int len = (int)nums.size(), l = 0, r = len - 1, m = 0;
+		vector<int> ans{ -1, -1 };
+		while (l <= r) {
+			m = l + (r - l) / 2;
+			if (target > nums[m]) l = m + 1;
+			else r = m - 1;
+		}
+		ans[0] = l, l = 0, r = len - 1;
+		while (l <= r) {
+			m = l + (r - l) / 2;
+			if (target < nums[m]) r = m - 1;
+			else l = m + 1;
+		}
+		ans[1] = r;
+		if (ans[1] < ans[0]) ans[0] = -1, ans[1] = -1;
+		return ans;
 	}
 };
