5fa254976bed039bfc290d90eb505fabddfbb74d
Hello Comrade <sun8726@hotmail.com>
2018-07-20 11:58:07 -0400

    Update leetcode 188

--- leetcode/BestTime2BuyandSellStock4.cpp
+++ leetcode/BestTime2BuyandSellStock4.cpp
@@ -45,6 +45,59 @@ Based upon the new comment in leetcode 122, even though we can't eliminate temp 
 to grab the bestBuy[j - 1] in temp variable so it can be used for the next iteration. This will need two temp variables t and t1.
 
 With the prove in leetcode 122, we can grab bestBuy[j - 1] in temp varaiable t and use it directly to calculate maxThere.
+
+***Update on 7/20/2018***
+
+Since I was redoing this series and buy the idea from:
+
+https://leetcode.com/articles/best-time-to-buy-and-sell-stock-with-transaction-fee/
+
+and applied it on leetcode 123, it's straightforward to follow the same thought for this one, which
+gives me an O(kN) time and O(k) space solution. No temp variable is necessary as usual.
+
+At each given p, we look for the max hold and cash if we have 1, 2, 3,..., k transcations occurred.
+
+we update the cash and hold array from k to 0. Therefore, no temp variable is needed and no
+worry for overwrite.
+
+One may ask: how could this work, say, kcash[j], when there is only m prices scanned, given m / 2 < j? Should we restrict
+inner loop to an extent that the jth transcation is possible?
+
+Answer is: not necessary at all since kcash array is initialized with 0 and khold array is set with INT_MIN. Therefore, if khold[j]
+is not valid yet, there is no way kcash[j] can be updated with a value greater than zero.
+
+```c++
+for (int p : prices) {
+for (int i = k - 1; i > -1; --i) {
+kcash[i] = std::max(kcash[i], khold[i] + p);
+khold[i] = std::max(khold[i], 0 == i ? -1LL * p : kcash[i - 1] - p);
+}
+}
+```
+Two edge cases I failed:
+
+1. k is extremely huge, bigger than the prices array, for example:
+
+1000000000
+[1]
+
+If we don't handle it, Runtime Error will occur: "terminate called after throwing an instance of 'std::bad_alloc' what():  std::bad_alloc".
+This is because stack can't allocate a long long array with the given size.
+The safeguard here is to check k and make sure k should be no greater than the size of the prices:
+
+k = std::min(k, len)
+
+I know this doesn't "fix" the issue, but will pass all test cases. In fact, there is more generic way, see next.
+
+2. After fixing the case above, we have solution accepted but it's extremely slow, which means there is a bias on given test cases. After reading
+the previous implementation, I realize the fact that:
+
+If k > sizeof(prices) / 2, the problem can be transformed to leetcode 122, which is "complete as many transactions as you like".
+In this scenario, it can be done in O(N) not O(kN). In given test cases, if k is extremely big, say, close to N, we should experience
+poor performance.
+
+So, by adding the check first (k >= len / 2), we have the opportunity to solve this much quick for these cases. In fact, this also
+cover the first edge case to eliminate the possible bad_alloc.
 */
 class SolutionBestTime2BuyandSellStock4 {
 private:
@@ -56,6 +109,23 @@ private:
 	}
 public:
 	int maxProfit(int k, vector<int>& prices) {
+		int len = (int)prices.size();
+		//k = std::min(k, len);
+		if (k >= len / 2) {
+			long long ans = 0;
+			for (int i = 1; i < len; ++i) ans += std::max(prices[i] - prices[i - 1], 0);
+			return static_cast<int>(ans);
+		}
+		vector<long long> khold(k, numeric_limits<int>::min()), kcash(k, 0);
+		for (int p : prices) {
+			for (int i = k - 1; i > -1; --i) {
+				kcash[i] = std::max(kcash[i], khold[i] + p);
+				khold[i] = std::max(khold[i], 0 == i ? -1LL * p : kcash[i - 1] - p);
+			}
+		}
+		return k > 0 ? static_cast<int>(kcash[k - 1]) : 0;
+	}
+	int maxProfit1(int k, vector<int>& prices) {
 		int len = static_cast<int>(prices.size()), ans = 0, t = 0, maxThere = 0;
 		if (k >= len / 2) return this->aux(prices, len);
 		vector<int> bestBuys(len + 1, INT_MAX);
@@ -70,7 +140,7 @@ public:
 		}
 		return ans;
 	}
-	int maxProfit1(int k, vector<int>& prices) {
+	int maxProfit2(int k, vector<int>& prices) {
 		int len = static_cast<int>(prices.size()), ans = 0, t = 0, t1 = 0, maxThere = 0;
 		if (k >= len / 2) return this->aux(prices, len);
 		vector<int> bestBuys(len + 1, INT_MAX);
@@ -87,3 +157,61 @@ public:
 		return ans;
 	}
 };
+/*
+Test cases:
+
+2
+[2,4,1]
+2
+[3,2,6,5,0,3]
+3
+[3,2,6,5,0,3]
+4
+[3,2,6,5,0,3]
+0
+[1]
+1000000000
+[1]
+3
+[1,2]
+10
+[1]
+10
+[]
+4
+[1,3,2,0,0,1,1,2,2,3,4,2,1,1,5,3,2,4,3,4,2,1]
+5
+[1,1,1,1,1,1,1,1]
+2
+[1,4,1,5,2,9,8]
+6
+[1,4,1,5,2,9,8]
+6
+[32,27,56,13,42,67,88,86,45,29,18,22,44,55,23]
+9
+[32,27,56,13,42,67,88,86,45,29,18,22,44,55,23]
+1
+[1,0,1]
+1
+[3]
+
+Outputs:
+
+2
+7
+7
+7
+0
+0
+1
+0
+0
+12
+0
+11
+14
+141
+141
+1
+0
+*/
