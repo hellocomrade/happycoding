7b0c05a44f9a27f80c03308de7c83b6d2fa6e056
Hello Comrade <hellocomrade@users.noreply.github.com>
2018-08-16 14:16:12 -0400

    Update strStr.java

--- java/strStr.java
+++ java/strStr.java
@@ -1,5 +1,29 @@
 //https://leetcode.com/problems/implement-strstr/
 public class SolutionStrStr {
+    private int[] find_lpps(final String pattern) {
+        int len = pattern.length(), l = -1, r = 1;
+        int[] lpps = new int[len];
+        Arrays.fill(lpps, -1);
+        while(r < len) {
+            while(-1 < l && pattern.charAt(l) != pattern.charAt(r - 1)) l = lpps[l];
+            lpps[r++] = ++l;
+        }
+        return lpps;
+    }
+    public int strsSr(String haystack, String needle) {
+        int len = haystack.length(), nlen = needle.length(), i = 0, j = 0;
+        if(len > 0 && nlen > 0) {
+            int[] lpps = this.find_lpps(needle);
+            while (i < len) {
+                while (j > -1 && haystack.charAt(i) != needle.charAt(j)) j = lpps[j];
+                ++i;
+                ++j;
+                if (j == nlen)
+                    return i - j;
+            }
+        }
+        return 0 == nlen ? 0 : -1;
+    }
     private int[] lpps(String needle) {
         if(0 == needle.length())return null;
         int len = needle.length();
@@ -22,7 +46,7 @@ public class SolutionStrStr {
         return ans;
     }
     //KMP version
-    public int strStr(String haystack, String needle) {
+    public int strStr0(String haystack, String needle) {
         if(null == haystack || null == needle || needle.length() > haystack.length())return -1;
         int hlen = haystack.length(), nlen = needle.length(), ans = 0, i = 0;
         int[] memo = this.lpps(needle);
