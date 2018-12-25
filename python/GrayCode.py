45f95b23443f06222ab4b11ba71c9c41d3a52380
Hello Comrade <hellocomrade@users.noreply.github.com>
2018-12-25 14:53:48 -0500

    Update GrayCode.py

--- python/GrayCode.py
+++ python/GrayCode.py
@@ -1,13 +1,42 @@
 # https://leetcode.com/problems/gray-code/
-#
-# Use keyword nonlocal to access outter variable that is changed internally
-# https://www.python.org/dev/peps/pep-3104/
 class SolutionGrayCode:
+    # Iterative
+    def grayCode(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        ans = [0]
+        for i in range(n):
+            ans += [1 << i | j for j in reversed(ans)]
+        return ans
+    
+    # Backtracing
+    # Use keyword nonlocal to access outter variable that is changed internally
+    # https://www.python.org/dev/peps/pep-3104/
+    def grayCode0(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        ans, i = [], 0
+        def fun(n):
+            nonlocal i
+            if 0 == n:
+                ans.append(i)
+            else:
+                fun(n - 1)
+                i ^= 1 << (n - 1)
+                fun(n - 1)
+        
+        fun(n)
+        return ans
+
     """
     @param n: a number
     @return: Gray code
     """
-    def grayCode(self, n):
+    def grayCode1(self, n):
         i, ans = 0, []
         def fun(idx):
             nonlocal i
@@ -19,3 +48,4 @@ class SolutionGrayCode:
                 fun(idx + 1)
         fun(0)
         return ans
+ 
