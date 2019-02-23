3e70141812b62e4ec9b09696d68321bf1aeef955
Hello Comrade <hellocomrade@users.noreply.github.com>
2019-02-22 20:33:03 -0500

    Update and rename Search4Range.cpp to FindFirstLastPosOfElemInSortedArray.cpp

--- leetcode/FindFirstLastPosOfElemInSortedArray.cpp
+++ leetcode/FindFirstLastPosOfElemInSortedArray.cpp
@@ -1,10 +1,11 @@
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-//https://leetcode.com/problems/search-for-a-range/
+//https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
 /*
-34. Search for a Range
+34. Find First and Last Position of Element in Sorted Array (Search for a Range)
 
 Given a sorted array of integers, find the starting and ending position of a given target value.
 
@@ -65,6 +66,42 @@ public:
 		}
 		return range;
 	}
+	//C++ lower_bound, upper_bound version
+	vector<int> searchRange(vector<int>& nums, int target) {
+            int l = 0, len = (int)nums.size(), r = len - 1, m = 0;
+            vector<int> ans {-1, -1};
+            auto itor = std::lower_bound(nums.begin(), nums.end(), target);
+            if (nums.end() != itor && target == *itor) {
+                ans[0] = itor - nums.begin();
+                itor = std::upper_bound(itor + 1, nums.end(), target);
+                ans[1] = itor - nums.begin() - 1;
+            }
+            return ans;
+        }
+	vector<int> searchRange00(vector<int>& nums, int target) {
+            int l = 0, len = (int)nums.size(), r = len - 1, m = 0;
+            vector<int> ans(2, 0);
+            ans[0] = len, ans[1] = -1;
+            while(l <= r) {
+                m = l + (r - l) / 2;
+                if(target >= nums[m]) {
+                    if(target == nums[m]) ans[1] = std::max(ans[1], m);
+                    l = m + 1;
+                }
+                else r = m - 1;
+            }
+            l = 0, r = len - 1;
+            while(l <= r) {
+                m = l + (r - l) / 2;
+                if(target <= nums[m]) {
+                    if(target == nums[m]) ans[0] = std::min(ans[0], m);
+                    r = m - 1;
+                }
+                else l = m + 1;
+            }
+            if(len == ans[0]) ans[0] = -1;
+            return ans;
+        }
 	vector<int> searchRange1(vector<int>& nums, int target) {
 		int len = (int)nums.size();
 		vector<int> ans{ -1, -1 };
@@ -93,3 +130,64 @@ public:
 		return ans;
 	}
 };
+/*
+Test cases:
+
+[5,7,7,8,8,10]
+8
+[5,7,7,8,8,10]
+5
+[5,7,7,8,8,10]
+10
+[5,7,7,8,8,10]
+1
+[1,2,3,4]
+2
+[1,2,3,4]
+3
+[1,2,3,4]
+1
+[1,2,3,4]
+4
+[1,2,3,4]
+0
+[1,2,3,4]
+5
+[1,2]
+1
+[1,2]
+2
+[1,2]
+0
+[1,2]
+3
+[]
+1
+[1]
+2
+[1]
+0
+[1]
+1
+
+Outputs:
+
+[3,4]
+[0,0]
+[5,5]
+[-1,-1]
+[1,1]
+[2,2]
+[0,0]
+[3,3]
+[-1,-1]
+[-1,-1]
+[0,0]
+[1,1]
+[-1,-1]
+[-1,-1]
+[-1,-1]
+[-1,-1]
+[-1,-1]
+[0,0]
+*/
