e3f7b6acaac8db5e353f2d88c70fae879ce89095
hellocomrade <sun8726@hotmail.com>
2020-01-13 10:57:27 -0500

    Update leetcode 560

--- leetcode/SubarraySumEqualsK.cpp
+++ leetcode/SubarraySumEqualsK.cpp
@@ -1,6 +1,5 @@
 #include <vector>
 #include <unordered_map>
-#include <numeric>
 
 using namespace std;
 
@@ -19,6 +18,13 @@ Output: 2
 Note:
 - The length of the array is in range [1, 20,000].
 - The range of numbers in the array is [-1000, 1000] and the range of the integer k is [-1e7, 1e7].
+
+Hints:
+
+- Will Brute force work here? Try to optimize it.
+- Can we optimize it by using some extra space?
+- What about storing sum frequencies in a hash table? Will it be useful?
+- sum(i,j)=sum(0,j)-sum(0,i), where sum(i,j) represents the sum of all the elements from index i to j-1. Can we use this property to optimize it.
 
 Observations:
 
@@ -40,15 +46,38 @@ public:
 		}
 		return ans;
 	}
-	int subarraySum0(vector<int>& nums, int k) {
-		int ans = 0;
-		std::partial_sum(nums.begin(), nums.end(), nums.begin());
-		unordered_map<int, int> memo;
-		++memo[0];
-		for (int i : nums) {
-			if (0 < memo.count(i - k)) ans += memo[i - k];
-			++memo[i];
-		}
-		return ans;
-	}
 };
+/*
+Test cases:
+
+[1,1,1]
+2
+[1]
+1
+[-11,-8,6,2,-2,10,5,6,-4,-7]
+0
+[0,0,0,0,0,0,0,0,0,0]
+0
+[0,0,0]
+0
+[1]
+1
+[1]
+0
+[1,-1,1,-1,1,1,2]
+2
+[1,-1,1,-1,1,1,1,2]
+2
+
+Outputs:
+
+2
+1
+3
+55
+6
+1
+0
+4
+7
+*/
