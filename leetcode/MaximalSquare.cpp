3c153891c31feb5defae672c8a51c9877f8522f9
hellocomrade <sun8726@hotmail.com>
2020-03-06 14:47:52 -0500

    Update leetcode 221

--- leetcode/MaximalSquare.cpp
+++ leetcode/MaximalSquare.cpp
@@ -35,17 +35,55 @@ guarantee forming the largest square with bottom right corner at [i, j].
 
 Official solution though is simpler: https://leetcode.com/problems/maximal-square/solution/
 
-Tracking on row and column counts are not necessary at all. memo[k] represents the linear size of the max
-square end at [i, k] as bottom right corner. The above formula still works but with a different meaning:
+using 1D rolling array.
+
+memo[k] represents the linear size of the max square end at [i, k] as bottom right corner. In order to decide
+memo[k] (the largest square ends up at matrix[i][k]), we will have to look at:
+
+1. the largest square ends at matrix[i - 1][k] (top);
+2. the largest square ends at matrix[i][k - 1] (left);
+3. the largest square ends at matrix[i - 1][k - 1] (diagonal);
+
+The smallest of 3 plus 1 will be the value of memo[k] if matrix[i][k] is '1'.
+
+Can check on diagonal be ignored? See this example:
+
+[
+[0, 1],
+[1, 1]
+]
+
+If only examining top and left, memo[1][1] = 2, which is wrong...
+
+The above formula still works:
 
 memo[j + 1] = '1' == matrix[i][j] ? std::min(d, std::min(memo[j], memo[j + 1])) + 1 : 0;
 
 It means the square size at [i, j] is decided by the smallest among the square ends at memo[j - 1] (current line, left),
 memo[j] (previous line, top) and memo[j - 1] (previous line, diagonal, top left).
+
+***Update on 2020-03-06***
+
+This question was mentioned in an entry level Amazon interview:
+
+https://leetcode.com/discuss/interview-experience/529395/amazon-sde1-seattle-february-2020-reject
 */
 class SolutionMaximalSquare {
 public:
 	int maximalSquare(vector<vector<char>>& matrix) {
+		int sz = (int)matrix.size();
+		if (0 == sz) return 0;
+		int len = (int)matrix[0].size(), ans = 0;
+		vector<int> memo(len + 1, 0);
+		for (int i = 0; i < sz; ++i)
+			for (int j = 1, prev = memo[0], tmp; j <= len; prev = tmp, ++j) {
+				tmp = memo[j];
+				memo[j] = ('1' == matrix[i][j - 1]) ? std::min(prev, std::min(memo[j - 1], memo[j])) + 1 : 0;
+				ans = std::max(ans, memo[j]);
+			}
+		return ans * ans;
+	}
+	int maximalSquare0(vector<vector<char>>& matrix) {
 		int ans = 0, rlen = (int)matrix.size();
 		if (1 > rlen) return ans;
 		int clen = (int)matrix[0].size();
@@ -87,6 +125,8 @@ Test cases:
 [["1","1","1"],["1","1","1"],["1","1","1"]]
 [["1","0","0"],["0","1","0"],["0","0","1"]]
 [["1","0","1","0","0"],["1","1","1","1","1"],["1","1","1","1","1"],["1","1","1","1","0"]]
+[["1","1","1","1","1","1","1","1"],["1","1","1","1","1","1","1","0"],["1","1","1","1","1","1","1","0"],["1","1","1","1","1","0","0","0"],["0","1","1","1","1","0","0","0"]]
+[["1","0","1","0"],["1","0","1","1"],["1","0","1","1"],["1","1","1","1"]]
 
 Outputs:
 
@@ -98,4 +138,6 @@ Outputs:
 9
 1
 9
+16
+4
 */
