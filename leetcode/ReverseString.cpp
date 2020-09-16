38d0164e483e2187bdd655ed7808d53f13570dc5
Hello Comrade <sun8726@hotmail.com>
2020-09-16 17:51:34 -0400

    Test new environment using leetcode 344

---
+++ leetcode/ReverseString.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+
+using namespace std;
+
+//https://leetcode.com/problems/reverse-string/
+/*
+344. Reverse String
+
+Write a function that reverses a string. The input string is given as an array of characters char[].
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+You may assume all the characters consist of printable ascii characters.
+
+Example 1:
+
+
+Input: ["h","e","l","l","o"]
+
+Output: ["o","l","l","e","h"]
+
+Example 2:
+
+Input: ["H","a","n","n","a","h"]
+
+Output: ["h","a","n","n","a","H"]
+*/
+class SolutionReverseString {
+public:
+	void reverseString(vector<char>& s) {
+		int i = 0, j = s.size() - 1;
+		while (i < j)
+			std::swap(s[i++], s[j--]);
+	}
+};
