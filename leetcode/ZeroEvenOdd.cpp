32a1bc4c1cf8fafa493546e1c16ba9be1a99a4f1
Hello Comrade <sun8726@hotmail.com>
2021-02-24 13:59:58 -0500

    Update on leetcode 1116

--- leetcode/ZeroEvenOdd.cpp
+++ leetcode/ZeroEvenOdd.cpp
@@ -50,7 +50,13 @@ Semaphore would be my first choice for this, however it's a C++20 feature and it
 
 The second option is mutex/condition_variable. I have seen most of implementation using only 1 lock. However, some of them are based upon the assumption that waiting thread can
 pick up the lock before the thread zero regain the lock (in those impl and mine, the for loop in zero controls the value for printing), which is wrong. Since the lock in zero is in a
-for loop, it's possible zero may regain the lock before other threads wake up. In that case, other threads will have to keep waiting until the lock can be acquired.
+for loop, it's possible zero may regain the lock before other threads wake up. In that case, other threads will have to keep waiting until the lock can be acquired, See ZeroEvenOdd2.
+
+****Update on 2-24-2021****
+The statement above is actually wrong. My colleague (https://github.com/kjmcclain) pointed out the second lock/condition is not necessary since the first lock/condition can be reused,
+if we have to something like ZeroEvenOdd2. See ZeroEvenOdd for the revised version. Also, he indicated that the unique_lock can be moved out of the for loop since wait will
+unlock the lock automatically. Combining these two points, ZeroEvenOdd looks much better if it still can be as fancy as ZeroEvenOdd1.
+
 
 A smarter way to do this, if only needs one mutex is run numbers (for loop) independently in all three threads. The condtion for waking up is changed accordingly. See ZeroEvenOdd1 I copied from someone on leetcode.
 
@@ -66,15 +72,9 @@ private:
 	bool next;
 	bool hasValue;
 	mutex mutex1;
-	mutex mutex2;
 	condition_variable condition1;
-	condition_variable condition2;
 public:
-	ZeroEvenOdd(int n) : mutex1(),
-		mutex2(),
-		condition1(),
-		condition2()
-	{
+	ZeroEvenOdd(int n) : mutex1(), condition1() {
 		this->n = n;
 		this->next = false;
 		this->hasValue = false;
@@ -83,33 +83,30 @@ public:
 
 	// printNumber(x) outputs "x", where x is an integer.
 	void zero(function<void(int)> printNumber) {
+		unique_lock<mutex> lck1(this->mutex1);
 		for (int i = 1; i <= this->n + 1; ++i) {
 			if (i <= n)
 				printNumber(0);
-			unique_lock<mutex> lck1(this->mutex1);
 			this->nextValue = i;
 			this->hasValue = true;
 			this->condition1.notify_all();
-			lck1.unlock();
 			if (i <= n) {
-				unique_lock<mutex> lck2(this->mutex2);
 				this->next = false;
-				this->condition2.wait(lck2, [this]() -> bool { return this->next; });
+				this->condition1.wait(lck1, [this]() -> bool { return this->next; });
 			}
 		}
 	}
 
 	void print(function<void(int)> printNumber, function<bool()> predicate) {
+		unique_lock<mutex> lck1(mutex1);
 		while (true) {
-			unique_lock<mutex> lck1(mutex1);
 			this->condition1.wait(lck1, [&]() -> bool { return this->hasValue == true && (predicate() == true || this->nextValue > this->n); });
 			if (this->nextValue > this->n)
 				break;
 			printNumber(this->nextValue);
 			this->hasValue = false;
-			unique_lock<mutex> lck2(this->mutex2);
 			this->next = true;
-			this->condition2.notify_all();
+			this->condition1.notify_all();
 		}
 	}
 
@@ -168,6 +165,69 @@ public:
 			lock.unlock();
 			cond.notify_all();
 		}
+	}
+};
+class ZeroEvenOdd2 {
+private:
+	int n;
+	int nextValue;
+	bool next;
+	bool hasValue;
+	mutex mutex1;
+	mutex mutex2;
+	condition_variable condition1;
+	condition_variable condition2;
+public:
+	ZeroEvenOdd2(int n) : mutex1(),
+		mutex2(),
+		condition1(),
+		condition2()
+	{
+		this->n = n;
+		this->next = false;
+		this->hasValue = false;
+		this->nextValue = 0;
+	}
+
+	// printNumber(x) outputs "x", where x is an integer.
+	void zero(function<void(int)> printNumber) {
+		unique_lock<mutex> lck1(this->mutex1, std::defer_lock);
+		for (int i = 1; i <= this->n + 1; ++i) {
+			if (i <= n)
+				printNumber(0);
+			lck1.lock();
+			this->nextValue = i;
+			this->hasValue = true;
+			this->condition1.notify_all();
+			lck1.unlock();
+			if (i <= n) {
+				unique_lock<mutex> lck2(this->mutex2);
+				this->next = false;
+				this->condition2.wait(lck2, [this]() -> bool { return this->next; });
+			}
+		}
+	}
+
+	void print(function<void(int)> printNumber, function<bool()> predicate) {
+		unique_lock<mutex> lck1(mutex1);
+		while (true) {
+			this->condition1.wait(lck1, [&]() -> bool { return this->hasValue == true && (predicate() == true || this->nextValue > this->n); });
+			if (this->nextValue > this->n)
+				break;
+			printNumber(this->nextValue);
+			this->hasValue = false;
+			unique_lock<mutex> lck2(this->mutex2);
+			this->next = true;
+			this->condition2.notify_all();
+		}
+	}
+
+	void even(function<void(int)> printNumber) {
+		this->print(printNumber, [this]() -> bool { return (this->nextValue & 1) == 0; });
+	}
+
+	void odd(function<void(int)> printNumber) {
+		this->print(printNumber, [this]() -> bool { return (this->nextValue & 1) == 1; });
 	}
 };
 /*
