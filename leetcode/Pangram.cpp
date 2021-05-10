6aba8cc14a9d4fd1d177b0fea604a966bac0fbb3
Hello Comrade <sun8726@hotmail.com>
2021-05-10 00:00:28 -0400

    Add leetcode 1832

---
+++ leetcode/Pangram.cpp
@@ -0,0 +1,44 @@
+#include <bitset>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+//https://leetcode.com/problems/check-if-the-sentence-is-pangram/
+/*
+1832. Check if the Sentence Is Pangram
+
+A pangram is a sentence where every letter of the English alphabet appears at least once.
+
+Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.
+
+
+
+Example 1:
+
+Input: sentence = "thequickbrownfoxjumpsoverthelazydog"
+
+Output: true
+
+Explanation: sentence contains at least one of every letter of the English alphabet.
+
+Example 2:
+
+Input: sentence = "leetcode"
+
+Output: false
+
+
+Constraints:
+
+- 1 <= sentence.length <= 1000
+- sentence consists of lowercase English letters.
+*/
+class SolutionPangram {
+public:
+	bool checkIfPangram(string sentence) {
+		std::bitset<26> memo;
+		std::for_each(sentence.begin(), sentence.end(), [&memo](const char& c) { if (memo.test((int)c - 97) == false)memo.set((int)c - 97); });
+		return memo.all();
+	}
+};
