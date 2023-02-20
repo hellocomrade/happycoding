a07d8b3cfd5eadb3c3b2f4383cb8ffc32d52f928
Hello Comrade <sun8726@hotmail.com>
2023-02-20 14:08:04 -0500

    Update MaxSubArray.cpp
    Update max-sub-array with divide and conquer

--- leetcode/MaxSubArray.cpp
+++ leetcode/MaxSubArray.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <limits>
 #include <algorithm>
+#include <functional>
 
 using namespace std;
 
@@ -16,6 +17,21 @@ If you have figured out the O(n) solution, try coding another solution using the
 Observations:
 So classic, have done multiple times here and there. But once for a while, I could easily forget the subtle details and take more than 15
 minutes to get this done, which is a shame...
+
+Update on 2023-02-20
+
+It has been a while...anyway, added divide-and-conquer version of max-sub-array. Given a 2-element-array, [a, b], if using divide-and-conquer,
+the max-sub-array, shall come from any of the following 3:
+
+1. [a]
+2. [b]
+3. [a + b]
+
+In other words, the sub-max coming from either left side or right side, or cross the boundary, in this case, it's (a + b).
+
+The first 2 cases can be covered intutively by divide-and-conquer, the third case, which is the max-sum of a sub sequence that guarantees covering the middle point,
+however, it's straightforward to calculate as well: starting from the middle point, scan toward left and get the max-sum in the range of [left, mid], scan toward right
+and get the amx-sum in the range of [mid, right]. Adding them togather gets the third case.
 */
 class SolutionMaximumSubArray {
 public:
@@ -36,5 +52,28 @@ public:
 			maxSofar = std::max(maxSofar, maxEndingHere);
 		}
 		return 0 == len ? 0 : static_cast<int>(maxSofar);
+	}
+	int maxSubArray_divide_conquer(vector<int>& nums) {
+		std::function<long long(int, int)> _maxSubArray = [&](int l, int r) -> long long {
+			if (l > r)
+				return numeric_limits<long long>::min();
+			if (l == r)
+				return nums[l];
+			int m = l + (r - l) / 2;
+			long long sum = 0LL;
+			long long lmax = numeric_limits<long long>::min();
+			long long rmax = numeric_limits<long long>::min();
+			for (int i = m; i >= l; --i) {
+				sum += nums[i];
+				lmax = std::max(lmax, sum);
+			}
+			sum = 0LL;
+			for (int i = m + 1; i <= r; ++i) {
+				sum += nums[i];
+				rmax = std::max(rmax, sum);
+			}
+			return std::max(lmax + rmax, std::max(_maxSubArray(l, m), _maxSubArray(m + 1, r)));
+		};
+		return (int)_maxSubArray(0, nums.size() - 1);
 	}
 };
